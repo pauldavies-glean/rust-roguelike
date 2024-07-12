b70c8214f097a6214a14412b719e3caf18957b15
Paul Davies <paul.davies@glean.co>
2024-07-12 14:03:59 +0100

    finished 3.1: nice walls with bitsets

--- src/map.rs
+++ src/map.rs
@@ -150,6 +150,51 @@ impl Map {
         !self.blocked[idx]
     }
 
+    fn is_revealed_and_wall(&self, x: i32, y: i32) -> bool {
+        let idx = self.xy_idx(x, y);
+        self.tiles[idx] == TileType::Wall && self.revealed_tiles[idx]
+    }
+
+    fn wall_glyph(&self, x: i32, y: i32) -> rltk::FontCharType {
+        if x < 1 || x > self.width - 2 || y < 1 || y > self.height - 2 as i32 {
+            return 35;
+        }
+        let mut mask: u8 = 0;
+
+        if self.is_revealed_and_wall(x, y - 1) {
+            mask += 1;
+        }
+        if self.is_revealed_and_wall(x, y + 1) {
+            mask += 2;
+        }
+        if self.is_revealed_and_wall(x - 1, y) {
+            mask += 4;
+        }
+        if self.is_revealed_and_wall(x + 1, y) {
+            mask += 8;
+        }
+
+        match mask {
+            0 => 9,    // Pillar because we can't see neighbors
+            1 => 186,  // Wall only to the north
+            2 => 186,  // Wall only to the south
+            3 => 186,  // Wall to the north and south
+            4 => 205,  // Wall only to the west
+            5 => 188,  // Wall to the north and west
+            6 => 187,  // Wall to the south and west
+            7 => 185,  // Wall to the north, south and west
+            8 => 205,  // Wall only to the east
+            9 => 200,  // Wall to the north and east
+            10 => 201, // Wall to the south and east
+            11 => 204, // Wall to the north, south and east
+            12 => 205, // Wall to the east and west
+            13 => 202, // Wall to the east, west, and south
+            14 => 203, // Wall to the east, west, and north
+            15 => 206, // ╬ Wall on all sides
+            _ => 35,   // We missed one?
+        }
+    }
+
     pub fn draw(&self, ctx: &mut Rltk) {
         let mut y = 0;
         let mut x = 0;
@@ -163,7 +208,7 @@ impl Map {
                         fg = RGB::from_f32(0.0, 0.5, 0.5);
                     }
                     TileType::Wall => {
-                        glyph = to_cp437('#');
+                        glyph = self.wall_glyph(x, y);
                         fg = RGB::from_f32(0., 1.0, 0.);
                     }
                     TileType::DownStairs => {
