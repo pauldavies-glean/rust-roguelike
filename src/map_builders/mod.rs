3ee144561b79aabfab2a369036d104fe3cdec932
Paul Davies <paul.davies@glean.co>
2024-09-06 14:58:34 +0100

    finished 4.8: DLA maps

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,6 +2,7 @@ mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod common;
+mod dla;
 mod drunkard;
 mod maze;
 mod simple_map;
@@ -11,6 +12,7 @@ use bevy_ecs::prelude::*;
 use bsp_dungeon::BspDungeonBuilder;
 use bsp_interior::BspInteriorBuilder;
 use cellular_automata::CellularAutomataBuilder;
+use dla::DLABuilder;
 use drunkard::DrunkardsWalkBuilder;
 use maze::MazeBuilder;
 use simple_map::SimpleMapBuilder;
@@ -26,7 +28,7 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 8);
+    let builder = rng.roll_dice(1, 11);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
@@ -35,6 +37,10 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
         5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
         6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
         7 => Box::new(MazeBuilder::new(new_depth)),
+        8 => Box::new(DLABuilder::walk_inwards(new_depth)),
+        9 => Box::new(DLABuilder::walk_outwards(new_depth)),
+        10 => Box::new(DLABuilder::central_attractor(new_depth)),
+        11 => Box::new(DLABuilder::insectoid(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
